fd47190a01860c9bbf04f8291dfae06681660de1
thapatech <ankitgodarthapa@gmail.com>
2021-12-12 22:59:12 +0545

    final commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Light Novel Scrapper
+
+A simple script that crawl throught websites and extract novel content in the form of .txt.
---
+++ index.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+
+      const novelName = "Grandson of the Holy Emperor is a Necromancer Chapter 1 to 100";
+      const starting = 1; // till 247
+      const totalChapter = 100;
+
+      for (let i = starting; i <= totalChapter; i++) {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+      
+            await page.goto(`https://www.lightnovelpub.com/novel/grandson-of-the-holy-emperor-is-a-necromancer/1270-chapter-${i}`);
+      
+            const grabBody = await page.$eval('div', (el) => el.innerText);
+      
+            const newChapter = `Chapter-${i}`+"\n\n"+grabBody+"\n\n";
+
+            // const newChapter = grabBody+"\n\n";
+
+            // const replaceTxt1 = await grabBody.replace('\nTranslator: Shiraishi  Editor: TheAlliance\n', ' ');
+      
+            // const replaceTxt2 = await replaceTxt1.replace('If you find any errors ( broken links, non-standard content, etc.. ), Please let us know so we can fix it as soon as possible.', '\n\n');
+      
+      
+            fs.appendFile(`${novelName}.txt`, newChapter, (err) => {
+                  if(err) throw console.log(`Error While Saving Chapter-${i}. Please Check If It Was Saved Correctly`);;
+                  console.log(`Chapter-${i} Successfullay Saved`);
+            });
+      
+            await browser.close();     
+      }
+})();
